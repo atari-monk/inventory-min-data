3d5dac6c4246fd6f8a583919b0422f02e418c85d
krzm <kmazanek@gmail.com>
2022-08-30 00:25:58 +0200

    rm flag

--- Inventory.Min.Data/Context/InventoryDbContext.cs
+++ Inventory.Min.Data/Context/InventoryDbContext.cs
@@ -12,24 +12,20 @@ public class InventoryDbContext
 	public DbSet<State>? State { get; set; }
 	public DbSet<Tag>? Tag { get; set; }
 	public DbSet<Unit>? Unit { get; set; }
-    public override bool UseLocalDbConfig { get; set; }
 
     public InventoryDbContext()
         : base()
     {
-        UseLocalDbConfig = true;
     }
 
     public InventoryDbContext(DbContextOptions<InventoryDbContext> options)
         : base(options)
     {
-        UseLocalDbConfig = false;
     }
 
     protected InventoryDbContext(DbContextOptions options)
         : base(options)
     {
-        UseLocalDbConfig = false;
     }
 
     protected override void SeedData(ModelBuilder builder)
