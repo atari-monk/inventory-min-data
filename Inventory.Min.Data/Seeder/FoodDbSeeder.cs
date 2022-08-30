790f50dfc97c07416c71da2d051fee36b869fc10
krzm <kmazanek@gmail.com>
2022-08-30 21:40:35 +0200

    Add testdb, fooddb seeders

---
+++ Inventory.Min.Data/Seeder/FoodDbSeeder.cs
@@ -0,0 +1,52 @@
+namespace Inventory.Min.Data;
+
+public class FoodDbSeeder
+    : InventorySeeder
+{
+    protected override Dictionary<string, object> CreateData()
+    {
+        return new Dictionary<string, object>()
+        {
+              { Category + "1", new Category { Id = 1, Name = "Food" } }
+            , { Category + "2", new Category { Id = 2, Name = "Furniture" } }
+            , { Category + "3", new Category { Id = 3, Name = "Pasta" } }
+            , { Currency + "1", new Currency { Id = 1, Name = "PLN" } }
+            , { Currency + "2", new Currency { Id = 2, Name = "EUR" } }
+            , { Currency + "3", new Currency { Id = 3, Name = "USD" } }
+            , { Unit + "1", new Unit { Id = 1, Name = "cm", Description = "Centimetre" } }
+            , { Unit + "2", new Unit { Id = 2, Name = "l", Description = "Litre" } }
+            , { Unit + "3", new Unit { Id = 3, Name = "g", Description = "Gram" } }
+            , { Unit + "4", new Unit { Id = 4, Name = "kg", Description = "Kilogram" } }
+            , { Tag + "1", new Tag { Name = "Preserves" } }
+            , { State + "1", new State { Id = 1, Name = "In storage" } }
+            , { State + "2", new State { Id = 2, Name = "In use" } }
+            , { State + "3", new State { Id = 3, Name = "Depleted" } }
+        };
+    }
+
+    public async Task Seed()
+    {
+        try
+        {
+            SeedCategory(Category + "1");
+            SeedCategory(Category + "2");
+            SeedCategory(Category + "3");
+            SeedCurrency(Currency + "1");
+            SeedCurrency(Currency + "2");
+            SeedCurrency(Currency + "3");
+            SeedUnit(Unit + "1");
+            SeedUnit(Unit + "2");
+            SeedUnit(Unit + "3");
+            SeedUnit(Unit + "4");
+            SeedTag(Tag + "1");
+            SeedState(State + "1");
+            SeedState(State + "2");
+            SeedState(State + "3");
+            await Context.SaveChangesAsync();
+        }
+        finally
+        {
+            await Context.DisposeAsync();
+        }
+    }
+}
--- Inventory.Min.Data/Seeder/InventorySeeder.cs
+++ Inventory.Min.Data/Seeder/InventorySeeder.cs
@@ -1,64 +1,55 @@
 namespace Inventory.Min.Data;
 
-public class TestSeeder
+public abstract class InventorySeeder
 {
-    private readonly InventoryDbContext context;
+    protected const string Category = nameof(Item.Category);
+    protected const string Currency = nameof(Item.Currency);
+    protected const string Unit = "Unit";
+    protected const string Tag = nameof(Item.Tag);
+    protected const string State = nameof(Item.State);
+    protected readonly InventoryDbContext Context;
     private readonly Dictionary<string, object> data;
     private readonly Dictionary<string, object> actions;
 
-    public TestSeeder()
+    public InventorySeeder()
     {
-        context = new InventoryDbContext();
+        Context = new InventoryDbContext();
         data = CreateData();
         actions = CreateActions();
     }
 
-    private Dictionary<string, object> CreateData()
-    {
-        return new Dictionary<string, object>()
-        {
-            { "Category-1", new Category { Name = "Food" } }
-            , { "Currency-1", new Currency { Name = "PLN" } }
-            , { "Unit-1", new Unit { Name = "cm", Description = "centimeter" } }
-            , { "Unit-2", new Unit { Name = "m", Description = "meter" } }
-            , { "Unit-3", new Unit { Name = "ml", Description = "milliliter" } }
-            , { "Unit-4", new Unit { Name = "l", Description = "liter" } }
-            , { "Unit-5", new Unit { Name = "kg" } }
-            , { "Tag-1", new Tag { Name = "Groceries 08.2022" } }
-            , { "State-1", new State { Name = "In Storage" } }
-        };
-    }
+    protected abstract Dictionary<string, object> CreateData();
 
-    private Dictionary<string, object> CreateActions()
+    protected virtual Dictionary<string, object> CreateActions()
     {
         return new Dictionary<string, object>()
         {
-            { 
-                "Category"
+            {
+                Category
                 , new SeedAction<InventoryDbContext, Category>(
                     (context, entity) => { return context.Category?.FirstOrDefault(c => c.Name == entity.Name); }
                     , (context, entity) => context.Category?.Add(entity))
             }
-            , { 
-                "Currency"
+            , {
+                Currency
                 , new SeedAction<InventoryDbContext, Currency>(
                     (context, entity) => { return context.Currency?.FirstOrDefault(c => c.Name == entity.Name); }
                     , (context, entity) => context.Currency?.Add(entity))
             }
-            , { 
-                "Unit"
+            , {
+                Unit
                 , new SeedAction<InventoryDbContext, Unit>(
                     (context, entity) => { return context.Unit?.FirstOrDefault(c => c.Name == entity.Name); }
                     , (context, entity) => context.Unit?.Add(entity))
             }
-            , { 
-                "Tag"
+            , {
+                Tag
                 , new SeedAction<InventoryDbContext, Tag>(
                     (context, entity) => { return context.Tag?.FirstOrDefault(c => c.Name == entity.Name); }
                     , (context, entity) => context.Tag?.Add(entity))
             }
             , {
-                "State"
+                State
                 , new SeedAction<InventoryDbContext, State>(
                     (context, entity) => { return context.State?.FirstOrDefault(c => c.Name == entity.Name); }
                     , (context, entity) => context.State?.Add(entity))
@@ -76,11 +67,6 @@ public class TestSeeder
         return (TType)actions[key];
     }
 
-    public async Task<TResult> InvokeAction<TResult>(Func<Task<TResult>> action)
-    {
-        return await action();
-    }
-
     private void SeedEntity<TContext, TEntity>(
         TContext context
         , string key
@@ -94,64 +80,43 @@ public class TestSeeder
         }
     }
 
-    private void SeedCategory(string key)
+    protected void SeedCategory(string key)
     {
         SeedEntity<InventoryDbContext, Category>(
-                context
-                , key
-                , GetAction<SeedAction<InventoryDbContext, Category>>("Category"));
+            Context
+            , key
+            , GetAction<SeedAction<InventoryDbContext, Category>>(Category));
     }
 
-    private void SeedCurrency(string key)
+    protected void SeedCurrency(string key)
     {
         SeedEntity<InventoryDbContext, Currency>(
-            context
+            Context
             , key
-            , GetAction<SeedAction<InventoryDbContext, Currency>>("Currency"));
+            , GetAction<SeedAction<InventoryDbContext, Currency>>(Currency));
     }
 
-    private void SeedUnit(string key)
+    protected void SeedUnit(string key)
     {
         SeedEntity<InventoryDbContext, Unit>(
-            context
+            Context
             , key
-            , GetAction<SeedAction<InventoryDbContext, Unit>>("Unit"));
+            , GetAction<SeedAction<InventoryDbContext, Unit>>(Unit));
     }
 
-    private void SeedTag(string key)
+    protected void SeedTag(string key)
     {
         SeedEntity<InventoryDbContext, Tag>(
-            context
+            Context
             , key
-            , GetAction<SeedAction<InventoryDbContext, Tag>>("Tag"));
+            , GetAction<SeedAction<InventoryDbContext, Tag>>(Tag));
     }
 
-    private void SeedState(string key)
+    protected void SeedState(string key)
     {
         SeedEntity<InventoryDbContext, State>(
-            context
+            Context
             , key
-            , GetAction<SeedAction<InventoryDbContext, State>>("State"));
-    }
-
-    public async Task Seed()
-    {
-        try
-        {
-            SeedCategory("Category-1");
-            SeedCurrency("Currency-1");
-            SeedUnit("Unit-1");
-            SeedUnit("Unit-2");
-            SeedUnit("Unit-3");
-            SeedUnit("Unit-4");
-            SeedUnit("Unit-5");
-            SeedTag("Tag-1");
-            SeedState("State-1");
-            await context.SaveChangesAsync();
-        }
-        finally
-        {
-            await context.DisposeAsync();
-        }
+            , GetAction<SeedAction<InventoryDbContext, State>>(State));
     }
 }
