86a60275a56c523e2eb4311c929ae5c263611464
krzm <kmazanek@gmail.com>
2022-09-21 00:41:31 +0200

    Upd food seed

--- Inventory.Min.Data.Seed.App/Seeder/FoodDbSeeder.cs
+++ Inventory.Min.Data.Seed.App/Seeder/FoodDbSeeder.cs
@@ -7,30 +7,32 @@ public class FoodDbSeeder
     {
         return new Dictionary<string, object>()
         {
-              { Category + "1", new Category { Id = 1, Name = "Food" } }
-            , { Category + "2", new Category { Id = 2, Name = "Furniture" } }
-            , { Category + "3", new Category { Id = 3, Name = "Pasta" } }
-            , { Currency + "1", new Currency { Id = 1, Name = "PLN" } }
-            , { Currency + "2", new Currency { Id = 2, Name = "EUR" } }
-            , { Currency + "3", new Currency { Id = 3, Name = "USD" } }
-            , { Unit + "1", new Unit { Id = 1, Name = "cm", Description = "Centimetre" } }
-            , { Unit + "2", new Unit { Id = 2, Name = "l", Description = "Litre" } }
-            , { Unit + "3", new Unit { Id = 3, Name = "g", Description = "Gram" } }
-            , { Unit + "4", new Unit { Id = 4, Name = "kg", Description = "Kilogram" } }
+              { Category + "1", new Category { Name = "Food" } }
+            , { Category + "2", new Category { Name = "Furniture" } }
+            , { Category + "3", new Category { Name = "Pasta" } }
+            , { Category + "4", new Category { Name = "Container" } }
+            , { Currency + "1", new Currency { Name = "PLN" } }
+            , { Currency + "2", new Currency { Name = "EUR" } }
+            , { Currency + "3", new Currency { Name = "USD" } }
+            , { Unit + "1", new Unit { Name = "cm", Description = "Centimetre" } }
+            , { Unit + "2", new Unit { Name = "l", Description = "Litre" } }
+            , { Unit + "3", new Unit { Name = "g", Description = "Gram" } }
+            , { Unit + "4", new Unit { Name = "kg", Description = "Kilogram" } }
             , { Tag + "1", new Tag { Name = "Preserves" } }
-            , { State + "1", new State { Id = 1, Name = "In storage" } }
-            , { State + "2", new State { Id = 2, Name = "In use" } }
-            , { State + "3", new State { Id = 3, Name = "Depleted" } }
+            , { State + "1", new State { Name = "In storage" } }
+            , { State + "2", new State { Name = "In use" } }
+            , { State + "3", new State { Name = "Depleted" } }
         };
     }
 
-    public async Task Seed()
+    public virtual async Task Seed()
     {
         try
         {
             SeedCategory(Category + "1");
             SeedCategory(Category + "2");
             SeedCategory(Category + "3");
+            SeedCategory(Category + "4");
             SeedCurrency(Currency + "1");
             SeedCurrency(Currency + "2");
             SeedCurrency(Currency + "3");
